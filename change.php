d9b13a4ea8fda73bfa82bd720c1a93bd2a24e23c
PrestonePhan <35493735+PrestonePhan@users.noreply.github.com>
2018-05-29 09:14:08 -0500

    Add files via upload

---
+++ change.php
@@ -0,0 +1,11 @@
+<?php
+if ($_GET['action'] == 'Yes') {
+} else if ($_GET['action'] == 'No') {
+    header('Location: Site.html');
+    exit;
+} else {
+    header('Location: Site.html');
+    exit;
+}
+
+?>
